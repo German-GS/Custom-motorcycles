b198302aded2ed238235afb8337bf2cedac8d408
GermanGS <german.gs19@gmail.com>
2020-01-19 16:20:39 -0600

    add smooth scroll

---
+++ src/js/smooth.js
@@ -0,0 +1,10 @@
+var scroll = new SmoothScroll('a[href*="#offers"]',{
+    speed: 700
+});
+var scroll = new SmoothScroll('a[href*="#aboutUs"]',{
+    speed: 700
+});
+
+var scroll = new SmoothScroll('a[href*="#contactUs"]',{
+    speed: 700
+});
